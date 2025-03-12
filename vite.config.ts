109470dc52bbe416b5f8b90501f0dcf9d489b736
gaojian <A24619@autelsz.com>
2025-03-12 16:11:58 +0800

    打包成一个html文件

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -20,4 +21,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000
+  }
 })
